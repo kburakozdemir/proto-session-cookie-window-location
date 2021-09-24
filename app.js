f1110aade897bdc1cc8b2c46ff595e0c5bb46015
K. Burak Ozdemir <kburakozdemir@gmail.com>
2021-09-24 14:25:09 +0300

    Fix coding standards

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function checkACookieExists() {
-  if (document.cookie.split(';').some((item) => item.trim().startsWith('gtmlpp='))) {
+  if (
+    document.cookie.split(";").some((item) => item.trim().startsWith("gtmlpp="))
+  ) {
     return "true";
   } else {
     return "false";
@@ -8,30 +10,41 @@ function checkACookieExists() {
 
 function getCookie(name) {
   var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-      var c = ca[i];
-      while (c.charAt(0)==' ') c = c.substring(1,c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
   return null;
 }
 
-var checkE=checkACookieExists();
+var checkE = checkACookieExists();
 if (checkE == "false") {
   // cookie does not exist
   console.log("cookie does not exist");
   document.cookie = "gtmlpp=" + window.location.pathname;
-  document.getElementById("baslik").innerHTML =  "ilk geldiğim sayfa = " + getCookie("gtmlpp");
+  document.getElementById("baslik").innerHTML =
+    "ilk geldiğim sayfa = " + getCookie("gtmlpp");
 } else {
-
-  document.getElementById("baslik").innerHTML =  "ilk geldiğim sayfa = " + getCookie("gtmlpp");
+  document.getElementById("baslik").innerHTML =
+    "ilk geldiğim sayfa = " + getCookie("gtmlpp");
 }
 
 var elem = document.getElementById("ck");
 
 elem.innerHTML = "href = " + window.location.href;
-elem.innerHTML = elem.innerHTML + "<br/>" + "window.location.href = " + window.location.href;
-elem.innerHTML = elem.innerHTML + "<br/>" + "window.location.pathname = " + window.location.pathname;
-elem.innerHTML = elem.innerHTML + "<br/>" + "(query params) window.location.search = " + window.location.search;
-elem.innerHTML = elem.innerHTML + "<br/>" + "window.location.hash = " + window.location.hash;
+elem.innerHTML =
+  elem.innerHTML + "<br/>" + "window.location.href = " + window.location.href;
+elem.innerHTML =
+  elem.innerHTML +
+  "<br/>" +
+  "window.location.pathname = " +
+  window.location.pathname;
+elem.innerHTML =
+  elem.innerHTML +
+  "<br/>" +
+  "(query params) window.location.search = " +
+  window.location.search;
+elem.innerHTML =
+  elem.innerHTML + "<br/>" + "window.location.hash = " + window.location.hash;
